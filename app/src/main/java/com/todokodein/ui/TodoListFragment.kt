8352cef7db3b841aaf758807c25527fe1874178e
Max <maxim.ananin@bravedevelopers.com>
2019-12-08 22:06:44 +0400

    todotest

--- app/src/main/java/com/todokodein/ui/TodoListFragment.kt
+++ app/src/main/java/com/todokodein/ui/TodoListFragment.kt
@@ -1,14 +1,20 @@
 package com.todokodein.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.todokodein.R
 import com.todokodein.data.db.entity.TodoItemEntity
 import com.todokodein.ui.adapter.TodoListAdapter
 import com.todokodein.ui.base.ScopedFragment
+import kotlinx.android.synthetic.main.dialog.view.*
+import kotlinx.android.synthetic.main.todo_list_fragment.*
+import kotlinx.coroutines.launch
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
 import org.kodein.di.generic.instance
@@ -27,5 +33,52 @@ class TodoListFragment : ScopedFragment(), KodeinAware, TodoListAdapter.TodoList
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.todo_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(TodoViewModel::class.java)
+
+        fetchData()
+    }
+
+    private fun fetchData() = launch {
+        val todoList = viewModel.todoList.await()
+        todoList.observe(this@TodoListFragment, Observer {
+            if (it == null) return@Observer
+            todoListData = todoList
+
+            bindUI()
+        })
+    }
+
+    private fun bindUI() {
+        when (todoListData.value!!.isEmpty()) {
+            true -> {
+                tvPlaceHolder.visibility = View.GONE
+            }
+            else -> {
+                val adapter = activity?.let { TodoListAdapter(it, todoListData) }
+                adapter?.setAdapterListener(this)
+                lvTodos.adapter = adapter
+                tvPlaceHolder.visibility = View.GONE
+                lvTodos.visibility = View.VISIBLE
+            }
+        }
+
+        fabAdd.setOnClickListener {
+            showDialog(null)
+        }
+    }
+
+    private fun showDialog(item: TodoItemEntity?) {
+        val inflater = activity?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val dialogLayout = inflater.inflate(R.layout.dialog, null)
+        dialogLayout.etTitle.setText(item?.title)
+
+
     }
 }
